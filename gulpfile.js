b377a0d146e45b38e416f4680cc8da95e4e81ea2
sopel1996 <sopel@live.ru>
2021-08-07 15:14:02 +0300

    HTML done. Baase css desktop done

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+// const autoprefixer = require('gulp-autoprefixer');
+// const postcss = require('gulp-postcss');
+
+function style() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass())
+        // .pipe(autoprefixer({
+        //     cascade: false
+        // }))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
+
+
