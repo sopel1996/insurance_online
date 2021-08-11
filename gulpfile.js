9f412ce78e5b32f5ae9943a844f88d680ae0fbeb
sopel1996 <sopel@live.ru>
2021-08-11 23:43:27 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 
 function style() {
-    return gulp.src('./scss/**/style.scss')
+    return gulp.src('./scss/**/style*.scss')
         .pipe(sass())
-        
+
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
 }
